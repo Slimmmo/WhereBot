77fc5dff73edee280c0f58f4acd2e2f03a62664a
Slimmmo <danielksimpson@gmail.com>
2019-08-09 14:41:36 +1000

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+commonAbbrevs = {
+	'bp': 'kingsford - randwick city',
+	'bp/maccas': 'kingsford - randwick city',
+	'maccas': 'kingsford - randwick city',
+	'gate9': 'manu et mente',
+	'gate 9': 'manu et mente',
+	'mem': 'manu et mente',
+	'll': 'unsw library lawn',
+	'mb': 'unsw metal ball sculpture',
+	'mhd': 'museum of human disease',
+	'nc': 'new college',
+	'nll': 'naked lady lawn',
+	'nmb': 'nelson mandela bust',
+	'tyree': 'unsw tyree building'
+}
+
+reloadRole = 'Mod'
